4070d94db5bbd85fa18d5a0355b4ded9ed60bd22
Easy Learn Java <easylearnjava4u@gmail.com>
2018-04-21 07:38:39 -0500

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -7,6 +7,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+
 @RestController
 public class GreetingController {
 
@@ -15,9 +24,29 @@ public class GreetingController {
 
     @CrossOrigin(origins = "http://localhost:9000")
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
+    public List<RecognitionResult> greeting(@RequestParam(required=false, defaultValue="World") String name) {
         System.out.println("==== in greeting ====");
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        
+        String theUrl = "http://35.193.175.155:8080/api/recognitions";
+        HttpHeaders headers = createHttpHeaders();
+        RestTemplate restTemplate = new RestTemplate();     
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        ResponseEntity<List<RecognitionResult>> response = restTemplate.exchange(theUrl, HttpMethod.GET, entity, 
+        		new ParameterizedTypeReference<List<RecognitionResult>>() {});
+        System.out.println("Result - status ("+ response.getStatusCode() + ") has body: " + response.getBody());
+        System.out.println("Result - status ("+ response.getStatusCode() + ") has body: " + response.getBody().size());
+    
+        List<RecognitionResult> rectempList = new ArrayList<RecognitionResult>();
+        int listSize = recList.size(); 
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!11 : " + listSize);
+        for(RecognitionResult obj : response.getBody()) {
+        	if(!recList.contains(obj.getId())) {
+        		recList.add(obj.getId());
+        		rectempList.add(obj);
+        	}
+        }
+        
+        return rectempList;        
     }
 
     @GetMapping("/greeting-javaconfig")
